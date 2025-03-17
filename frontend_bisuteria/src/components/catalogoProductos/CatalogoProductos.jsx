0b12523f7ba317567d2ec70ae1b2c3af164eff32
AngelHA357 <josehuerta357@outlook.com>
2025-03-17 16:06:25 -0700

    Componente de TarjetaProducto hecho y se muestran

--- frontend_bisuteria/src/components/catalogoProductos/CatalogoProductos.jsx
+++ frontend_bisuteria/src/components/catalogoProductos/CatalogoProductos.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { BarraNavegacion } from './BarraNavegacion';
 import { FiltroCategorias } from './FiltroCategorias';
+import productos from '../../mocks/productos.json'
 import './estilos/catalogoStyles.css';
+import { TarjetaProducto } from './TarjetaProducto';
 
 export function CatalogoProductos() {
+    const [productosState] = useState(productos)
+    const [filtros, setFiltros] = useState({
+        categoria: 'todas'
+    }) 
+
+
+    const filtrarProductos = (productos) => {
+        return productos.filter(producto => {
+            return (
+                filtros.categoria === 'todas' ||
+                producto.categoria === filtros.categoria
+            )
+        })
+    }
+
+    const productosFiltrados = filtrarProductos(productosState)
+
     const handleCartClick = () => {
         console.log('Carrito clicked');
     };
@@ -21,6 +40,18 @@ export function CatalogoProductos() {
             />
             <FiltroCategorias
             />
+            <div className='product-list'>
+            {productosFiltrados.map((producto) => (
+                
+                <TarjetaProducto 
+                key={producto.id}
+                image={producto.imagenes[0]}
+                name={producto.nombre}
+                price={producto.precio}
+                category={producto.categoria}
+                />        
+            ))}
+            </div>
         </>
     );
 }
