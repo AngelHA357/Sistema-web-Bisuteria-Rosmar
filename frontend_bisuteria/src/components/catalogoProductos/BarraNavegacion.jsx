5f824250440c71bf514ec2bbf70d96597537799e
AngelHA357 <josehuerta357@outlook.com>
2025-03-23 21:09:31 -0700

    Conexión con backend en clientes y productos
    Ya se acceden a usuarios desde el front al back, y se hizo la pantalla de registrar producto. Faltan detalles como mejorar su estilo y el tema de los colores que están fijos.

--- frontend_bisuteria/src/components/catalogoProductos/BarraNavegacion.jsx
+++ frontend_bisuteria/src/components/catalogoProductos/BarraNavegacion.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logoBisuteria from '/imgs/logo_bisuteria_acceso_usuario.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import './estilos/barraNavStyles.css';
+import { UserContext } from '../../context/UserContext';
 
-export function BarraNavegacion({ cartItemsCount, onCartClick, onAccountClick }) {
-    return (
-      <header className="barraNav">
-        <img src={logoBisuteria} alt="Logo Rosmar Bisutería" className="logo" />
-        <div className="user-actions">
-          <button onClick={onCartClick}>
+export function BarraNavegacion() {
+const { logout } = useContext(UserContext);
+  return (
+    <header className="barraNav">
+      <img src={logoBisuteria} alt="Logo Rosmar Bisutería" className="logo" />
+      <div className="user-actions">
+        <button>
           <FontAwesomeIcon icon={faShoppingCart} />
-            Carrito {cartItemsCount > 0 && `(${cartItemsCount})`}
-          </button>
-          <button onClick={onAccountClick} ><FontAwesomeIcon icon={faCircleUser} /> Mi cuenta</button>
-        </div>
-      </header>
-    );
-  }
+          Carrito
+        </button>
+        <button onClick={logout} ><FontAwesomeIcon icon={faCircleUser} /> Mi cuenta</button>
+      </div>
+    </header>
+  );
+}
